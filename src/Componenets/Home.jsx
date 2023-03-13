b5a282d44bd2a9383cfa0312e4ec6a755cb5a77b
Areebey <areebrafiq125@gmail.com>
2023-03-14 02:50:03 +0500

    new commit

--- src/Componenets/Home.jsx
+++ src/Componenets/Home.jsx
@@ -1,23 +1,40 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import Task from './Task';
 
 
 
 
 function Home() {
+   const [tasks,setTasks]=useState([]);
+   const [tittle,setTittle]=useState("");
+   const [description,setDescription]=useState("");
+console.log(tittle,description)
+
+
+const submitHandler = (e) =>{
+      e.preventDefault();
+        
+      setTasks([...tasks , {tittle,description}]);
+};
   return (
     <div className="container">
         <h1>Daily Goals</h1>
 
-        <form>
-            <input type="text" placeholder='Title' />
-            <textarea  id="" placeholder='Description'></textarea>
+        <form onSubmit={submitHandler}>
+            <input type="text" placeholder='Title' value={tittle} 
+              onChange={(e)=> setTittle (e.target.value)}/>
+
+            <textarea  id="" placeholder='Description' value={description} 
+              onChange={(e)=> setDescription(e.target.value)}></textarea>
 
             <button type="submit">ADD</button>
         </form>
 
-        <Task/>
+        {tasks.map((item,index) => (
+        <Task key={index} tittle={item.tittle} description={item.description} />
+        ))}
+            
     </div>
   )
 }
